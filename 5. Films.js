779cb8c80e731d6ac4bbe54818da02ef6de3f663
Grigoriy <thegrigoriy@mail.ru>
2020-05-17 16:20:22 +0300

    2 вопроса пользователю и присвоение значений переменным

--- 5. Films.js
+++ 5. Films.js
@@ -1,5 +1,24 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?'); 
-console.log(numberOfFilms); 
+// Необходимо задать пользователю 2 вопроса и занести его ответы в переменные
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); 
+//console.log(numberOfFilms); ''
+
+const personalMovieDB = {
+    coint: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+b = prompt('На сколько вы его оцените?',''),
+c = prompt('Один из последних просмотренных фильмов?',''),
+d = prompt('На сколько вы его оцените?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
