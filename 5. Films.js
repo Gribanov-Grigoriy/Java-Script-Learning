f97dabaf83d490f31b12cc906998ba7b82a1cbe5
Grigoriy <thegrigoriy@mail.ru>
2020-05-17 23:18:00 +0300

    Условия и циклы в 5. Films

--- 5. Films.js
+++ 5. Films.js
@@ -1,24 +1,43 @@
 'use strict';
 
-// Необходимо задать пользователю 2 вопроса и занести его ответы в переменные
-
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); 
 //console.log(numberOfFilms); ''
 
 const personalMovieDB = {
-    coint: numberOfFilms,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-b = prompt('На сколько вы его оцените?',''),
-c = prompt('Один из последних просмотренных фильмов?',''),
-d = prompt('На сколько вы его оцените?','');
+// const a = prompt('Один из последних просмотренных фильмов?',''),
+// b = prompt('На сколько вы его оцените?',''),
+// c = prompt('Один из последних просмотренных фильмов?',''),
+// d = prompt('На сколько вы его оцените?','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i=1; i<3; i++) {
+   const a = prompt('Один из последних просмотренных фильмов?','');
+   const b = prompt('На сколько вы его оцените?','');
+
+   if (a !=null && b !=null && a !='' && b !='' && a.length < 50) {    // a и b не равны null (отмене), и так далее. для И используем логическое И - &&
+       personalMovieDB.movies[a] = b;
+       console.log('done');
+   } else {
+       console.log('error');
+       i--;
+   }
+}
+
+if(personalMovieDB.count < 10) {
+    console.log("Маловато");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Ошибка");
+}
+
 
 console.log(personalMovieDB);
